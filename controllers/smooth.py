dd8a9dcbe7c72b01dd49ef5e6e7665af94d1677f
stilobique <aurelienvlt@free.fr>
2017-07-12 18:00:06 +0200

    Update the Smooth operator tu change the angle value on the Invoke operator (popup menu)

--- controllers/smooth.py
+++ controllers/smooth.py
@@ -1,13 +1,18 @@
 import bpy
 import math
 
+from bpy.props import FloatProperty
+
 
 class SmoothShading(bpy.types.Operator):
     """Activate the Auto Smooth with an 45° angle"""
     bl_idname = "object.smooth_shading"
     bl_label = "Advanced Smooth Shading"
+    bl_options = {'REGISTER', 'UNDO'}
 
+    angle = FloatProperty(name="Angle Value", min=0, max=180, subtype='ANGLE')
 
+    @staticmethod
     def execute(self, context):
         sltd_obj = context.selected_objects
         data = bpy.data
@@ -18,10 +23,15 @@ class SmoothShading(bpy.types.Operator):
                 bpy.ops.object.shade_smooth()
                 data.objects[obj.name].data.use_auto_smooth = True
                 mesh_name = data.objects[obj.name].data.name
-                angle = math.radians(45)
+                angle = self.angle
                 bpy.data.meshes[mesh_name].auto_smooth_angle = angle
 
         return {'FINISHED'}
+
+    def invoke(self, context, event):
+        self.angle = math.radians(15)
+
+        return self.execute(self, context)
 
 
 def register():
